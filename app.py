0dd21ab1d14354523a8e910c447b4afc1589aa0a
Stanley Yu <stanley98yu@gmail.com>
2018-09-06 01:11:52 -0400

    Completed challenge.

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import requests
+import json
 
 app = Flask(__name__)
 
@@ -7,6 +9,19 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route('/pokemon/<query>', methods=['GET'])
+def display_pokemon(query):
+    try:
+        id = int(query)
+        url = 'http://pokeapi.co/api/v2/pokemon/{0}'.format(id)
+        r = requests.get(url)
+        name = r.json()["name"]
+        return render_template('id.html', id=id, name=name)
+    except ValueError:
+        url = 'http://pokeapi.co/api/v2/pokemon/{0}'.format(query)
+        r = requests.get(url)
+        id = r.json()["id"]
+        return render_template('name.html', id=id, name=query)
 
 if __name__ == '__main__':
     app.run()
